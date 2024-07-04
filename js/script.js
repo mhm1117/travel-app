dd0ab86ad73b09327234809daadb53dd77d155b3
Matilda <mmichel@ucsd.edu>
2024-07-04 12:14:15 -0700

    added loggin in css - working on js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,16 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
 
+    getLogins()
+
+    
 }
 
+async function getLogins() {
+
+    const furl = "https://github.com/mhm1117/travel-app/blob/main/data/logins.txt";
+
+    const response = await fetch(furl);
+
+    console.log(response);
+}
