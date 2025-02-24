385aebab75f97b0e255b69439d6fe73e5c81a459
Matilda <mmichel@ucsd.edu>
2025-02-24 14:47:15 -0800

    updated to connected to online database

--- includes/dbhandling.inc.php
+++ includes/dbhandling.inc.php
@@ -1,8 +1,12 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=travelapp_db";
-$dbusername = "root";
-$dbpassword = "";
+// $dsn = "mysql:host=localhost;dbname=travelapp_db";
+// $dbusername = "root";
+// $dbpassword = "";
+
+$dsn = "mysql:host=209.58.148.242:3306;dbname=mattiem17_travelappdb";
+$dbusername = "mattiem17_mainuser";
+$dbpassword = "SnearKlens!17";
 
 try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
