e835a3d0a2c2efeffa59f73ce98595230b65a27e
Matilda <mmichel@ucsd.edu>
2025-04-18 16:48:20 -0700

    more styling for event list - not working for phone size

--- includes/events/getEvents_view.inc.php
+++ includes/events/getEvents_view.inc.php
@@ -1,10 +1,14 @@
 <div class="mx-auto w-75 d-grid row-gap-3 mb-3" id="events-list" style="max-width: 700px;">
     <div class="event-li row rounded py-2 bg-light bg-opacity-75 shadow-sm">
-        <div class="col d-flex flex-column fw-bold h-100 justify-content-between gap-1">
-            <div class="d-flex gap-2 text-start" style="margin-left: -1.35em; font-size: 1.2em;">
-                <span class="bg-maincolor rounded-circle" style="width: fit-content; padding-left: 0.5em; padding-right: 0.5em;">1</span>
-                <a class="link link-dark text-decoration-none">Pickleball</a>
+        <div class="col d-flex flex-column fw-bold h-100 justify-content-between gap-1 event-col-1">
+            <div class="d-flex flex-column justify-content-around align-items-start">
+                <div class="d-flex gap-2 text-start" style="margin-left: -1.35em; font-size: 1.2em;">
+                    <span class="bg-maincolor rounded-circle" style="width: fit-content; padding-left: 0.5em; padding-right: 0.5em;">1</span>
+                    <a class="link link-dark text-decoration-none">Pickleball</a>
+                </div>
+                <button type="button" class="ms-2 text-start btn btn-light fw-bold px-1 py-0" style="font-size: 0.85em;">San Francisco, CA</button>
             </div>
+            
             <div class="ms-2" style="font-size: 0.85em;">
                 <button type="button" class="btn btn-light bg-secondary-subtle px-1 py-0 text-start">
                     I go play pickleball at the presidio wall courts with Angie and Olivia.
@@ -15,16 +19,19 @@
                 <button type="button" class="btn btn-light fw-bold px-1 py-0" style="font-size: 0.85em;">1:30 PM - 2:30 PM</button>
             </div>
         </div>
-        <div class="col text-end d-flex flex-column justify-content-between gap-3">
-            <div class=""><span class="bg-danger-subtle rounded px-2">Activity</span></div>
-            <img src="../../assets/presidio-wall-courts.jpg" class="img-fluid rounded align-self-end" style="max-height: 150px;">
+        <div class="col text-end d-flex flex-column justify-content-between gap-3 event-col-2">
+            <div class=""><button type="button" class="btn btn-light bg-danger-subtle rounded px-2 py-0"  style="font-size: 1em;">Activity</button></div>
+            <img src="../../assets/presidio-wall-courts.jpg" class="img-fluid rounded align-self-end event-img" style="max-height: 150px;">
         </div>
     </div>
     <div class="event-li row rounded py-2 bg-light bg-opacity-75 shadow-sm">
         <div class="col d-flex flex-column fw-bold h-100 justify-content-between gap-1">
-            <div class="d-flex gap-2 text-start" style="margin-left: -1.35em; font-size: 1.2em;">
-                <span class="bg-maincolor rounded-circle" style="width: fit-content; padding-left: 0.5em; padding-right: 0.5em;">2</span>
-                <a class="link link-dark text-decoration-none">Beer Tasting Night</a>
+            <div class="d-flex flex-column justify-content-around align-items-start">
+                <div class="d-flex gap-2 text-start" style="margin-left: -1.35em; font-size: 1.2em;">
+                    <span class="bg-maincolor rounded-circle" style="width: fit-content; padding-left: 0.5em; padding-right: 0.5em;">2</span>
+                    <a class="link link-dark text-decoration-none">Beer Tasting Night</a>
+                </div>
+                <button type="button" class="ms-2 text-start btn btn-light fw-bold px-1 py-0" style="font-size: 0.85em;">San Francisco, CA</button>
             </div>
             <div class="ms-2" style="font-size: 0.85em;">
                 <button type="button" class="btn btn-light bg-secondary-subtle px-1 py-0 text-start">
@@ -37,9 +44,9 @@
                 <button type="button" class="btn btn-light fw-bold px-1 py-0" style="font-size: 0.85em;">6:00 PM - Add Time</button>
             </div>
         </div>
-        <div class="col text-end d-flex flex-column justify-content-between gap-3">
-            <div class=""><span class="bg-warning-subtle rounded px-2">Food</span></div>
-            <img src="../../assets/beer-tasting.jpg" class="img-fluid rounded align-self-end" style="max-height: 150px;">
+        <div class="col text-end d-flex flex-column justify-content-between gap-3 event-col-2">
+            <div class=""><button type="button" class="btn btn-light bg-warning-subtle rounded px-2 py-0" style="font-size: 1em;">Food</button></div>
+            <img src="../../assets/beer-tasting.jpg" class="img-fluid rounded align-self-end event-img" style="max-height: 150px;">
         </div>
         <!-- <div class="col"><span class="bg-danger-subtle rounded py-1 px-2">Activity</span></div> -->
     </div>
